dc1a597f1ce3e997dab1fad335157b1bcefb63a1
Mario Mip <mariomip98@gmail.com>
2026-01-04 18:40:43 +0000

    feat: Finished int and float works

--- int_and_float_work.py
+++ int_and_float_work.py
@@ -151,4 +151,52 @@ print(f'Result: {int_1 ** int_2}\n')
 print(f'Exponentiation float\n\t{float_1} ** {float_2}')
 print(f'Result: {float_1 ** float_2}\n')
 
+# Python allows to convert numbers to certain type
+# and also stringsprint('\n--- Int and Float conversion ---\n')
 
+int_1 = 56 
+float_1 = 12.92563
+
+print(f'Int to float with float()\n{int_1}\n{type(int_1)}\nFloat\n{float(int_1)}\n{type(float(int_1))}\n')
+print(f'Float to int with int()\n{float_1}\n{type(float_1)}\nInt\n{int(float_1)}\n{type(int(float_1))}\n')
+
+str_int = '45'
+str_float = '7.8'
+
+print('Now with strings\n')
+
+print(f'Converted int\n\'{str_int}\'\n{int(str_int)}\n{type(int(str_int))}\n')
+print(f'Converted float\n\'{str_float}\'\n{float(str_float)}\n{type(float(str_float))}\n')
+
+# Extra methods that we can use
+print('\n--- Extra methods ---\n')
+
+
+# round()
+# This rounds a number to the nearest
+# integer and returns with no decimals
+print('round()')
+
+int_1 = 4.798
+int_2 = 4.253
+
+print(f'Int 1:\n{int_1}\nround(int_1)\n{round(int_1)}\n')
+print(f'Int 2:\n{int_2}\nround(int_2, 1)\n{round(int_2, 1)}\n')
+
+
+# abs()
+# Returns the absolute value of a number
+print('abs()')
+
+num = -15
+
+print(f'Num:\n{num}\nabs(num)\n{abs(num)}\n')
+
+# pow()
+# Returns the raise of a number
+# to the power of another, or is a 
+# modular exponentiation
+print('pow()')
+
+print(f'Result 1:\npow(2, 3)\n{pow(2, 3)}\nEquivalent to 2 ** 3\n')
+print(f'Result 2:\npow(2, 3, 5)\n{pow(2, 3, 5)}\nEquivalent to (2 ** 3) % 5\n')
