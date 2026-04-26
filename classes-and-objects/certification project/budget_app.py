4b3517518d575040ba002adbcfdda0ba9a318784
Mario Meade <mariomip98@gmail.com>
2026-04-25 18:36:13 -0600

    feat: Added a __main__ to som projects

--- classes-and-objects/certification project/budget_app.py
+++ classes-and-objects/certification project/budget_app.py
@@ -79,13 +79,13 @@ def create_spend_chart(categories):
         output += line + "\n"
     return output.rstrip("\n")
 
-
-food = Category('Food')
-food.deposit(1000, 'initial deposit')
-food.withdraw(10.15, 'groceries')
-food.withdraw(15.89, 'restaurant and more food for dessert')
-clothing = Category('Clothing')
-food.transfer(50, clothing)
-print(food)
-print(clothing)
-print(create_spend_chart([food, clothing]))
+if __name__ == '__main__':
+    food = Category('Food')
+    food.deposit(1000, 'initial deposit')
+    food.withdraw(10.15, 'groceries')
+    food.withdraw(15.89, 'restaurant and more food for dessert')
+    clothing = Category('Clothing')
+    food.transfer(50, clothing)
+    print(food)
+    print(clothing)
+    print(create_spend_chart([food, clothing]))
